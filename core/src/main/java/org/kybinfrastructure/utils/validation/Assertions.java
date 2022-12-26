482e4075c515744dc39ec75c073bf33badbec17d
Onur Kayabasi <onurbpm@outlook.com>
2022-12-26 20:19:37 +0100

    feat: Assertions component started

---
+++ core/src/main/java/org/kybinfrastructure/utils/validation/Assertions.java
@@ -0,0 +1,38 @@
+package org.kybinfrastructure.utils.validation;
+
+/**
+ * Contains useful assertion methods
+ */
+public final class Assertions {
+	private Assertions() {}
+
+	/**
+	 * Checks whether the given object is not null.
+	 * 
+	 * @param value value to be checked
+	 * @param message message to be set as the {@link IllegalArgumentException} message if the given
+	 *        {@code value} is {@code null}
+	 * @throws IllegalArgumentException is thrown when the given {@code value} is {@code null}
+	 */
+	public static void notNull(Object value, String message) {
+		if (value == null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	/**
+	 * Checks whether the given {@code String} value contains at least one non space character.
+	 * 
+	 * @param value {@code String} value to be checked
+	 * @param message message to be set as the {@link IllegalArgumentException} message if the given
+	 *        {@code value} has no non space character
+	 */
+	public static void notBlank(String value, String message) {
+		if (value == null) {
+			throw new IllegalArgumentException(message);
+		}
+		if (value.trim().length() == 0) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+}
