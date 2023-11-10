fb02b259880ba6c1ad9e64d5dfe7af1fd15689b6
Onur Kayabasi <onurbpm@outlook.com>
2023-11-10 22:51:18 +0100

    refactor: ScannerClassgraphImpl refactored

--- core/src/main/java/org/kybinfrastructure/ioc/ScannerClassgraphImpl.java
+++ core/src/main/java/org/kybinfrastructure/ioc/ScannerClassgraphImpl.java
@@ -1,12 +1,11 @@
 package org.kybinfrastructure.ioc;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import org.kybinfrastructure.exception.UnexpectedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.github.classgraph.ClassGraph;
-import io.github.classgraph.ClassInfoList;
 import io.github.classgraph.ScanResult;
 
 final class ScannerClassgraphImpl implements Scanner {
@@ -17,19 +16,19 @@ final class ScannerClassgraphImpl implements Scanner {
   public Set<Class<?>> scan(Class<?> rootClass) {
     try (ScanResult scanResult =
         new ClassGraph().enableAllInfo().acceptPackages(rootClass.getPackageName()).scan()) {
-      ClassInfoList classesWithAnnotation = scanResult.getClassesWithAnnotation(Injector.class);
+      var classesWithInjectorAnnotation = scanResult.getClassesWithAnnotation(Injector.class);
 
-      Set<Class<?>> injectorClasses = new HashSet<>();
-      for (int i = 0; i < classesWithAnnotation.size(); i++) {
-        Class<?> injectorClass = classesWithAnnotation.get(i).loadClass();
+      Set<Class<?>> injectorClasses = new LinkedHashSet<>();
+      for (int i = 0; i < classesWithInjectorAnnotation.size(); i++) {
+        Class<?> injectorClass = classesWithInjectorAnnotation.get(i).loadClass();
 
-        LOGGER.debug("{} injector class was loaded!", injectorClass);
+        LOGGER.debug("{} injector class was loaded!", injectorClass.getName());
         injectorClasses.add(injectorClass);
       }
 
       return injectorClasses;
     } catch (Exception e) {
-      throw new UnexpectedException("Injectors couldn't be extracted!", e);
+      throw new UnexpectedException("Injector classes couldn't be extracted!", e);
     }
   }
 
--- core/src/test/java/org/kybinfrastructure/ioc/ScannerClassgraphImplTest.java
+++ core/src/test/java/org/kybinfrastructure/ioc/ScannerClassgraphImplTest.java
@@ -1,37 +1,45 @@
 package org.kybinfrastructure.ioc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.kybinfrastructure.exception.UnexpectedException;
+import example.service.SomeService;
+
 class ScannerClassgraphImplTest {
 
   private final ScannerClassgraphImpl underTest = new ScannerClassgraphImpl();
 
-  // @Test
-  // void scan_Returns_Injector_Classes_Under_GivenRootClass_Package() {
-  // // given
-  // Class<?> rootClass = SomeService.class;
+  @Test
+  void scan_Returns_Injector_Classes_Under_GivenRootClass_Package() {
+    // given
+    Class<?> rootClass = SomeService.class;
 
-  // // when
-  // Set<Class<? extends Injector>> actualResult = underTest.scan(rootClass);
+    // when
+    Set<Class<?>> actualResult = underTest.scan(rootClass);
 
-  // // then
-  // assertEquals(2, actualResult.size());
-  // assertTrue(actualResult.stream()
-  // .anyMatch(sc -> "example.service.ServiceInjector".equals(sc.getName())));
-  // assertTrue(actualResult.stream()
-  // .anyMatch(sc -> "example.service.sub.AnotherServiceInjector".equals(sc.getName())));
-  // }
+    // then
+    assertEquals(2, actualResult.size());
+    assertTrue(actualResult.stream()
+        .anyMatch(sc -> "example.service.ServiceInjector".equals(sc.getName())));
+    assertTrue(actualResult.stream()
+        .anyMatch(sc -> "example.service.sub.AnotherServiceInjector".equals(sc.getName())));
+  }
 
-  // @Test
-  // void scan_Throws_UnexpectedException_When_Unexpected_Exception_Occurred() {
-  // // given
-  // Class<?> rootClass = null;
+  @Test
+  void scan_Throws_UnexpectedException_When_Unexpected_Exception_Occurred() {
+    // given
+    Class<?> rootClass = null;
 
-  // // when
-  // UnexpectedException thrownException =
-  // assertThrows(UnexpectedException.class, () -> underTest.scan(rootClass));
+    // when
+    UnexpectedException thrownException =
+        assertThrows(UnexpectedException.class, () -> underTest.scan(rootClass));
 
-  // // then
-  // assertEquals("Injectors couldn't be extracted!", thrownException.getMessage());
-  // assertEquals(NullPointerException.class, thrownException.getCause().getClass());
-  // }
+    // then
+    assertEquals("Injector classes couldn't be extracted!", thrownException.getMessage());
+    assertEquals(NullPointerException.class, thrownException.getCause().getClass());
+  }
 
 }
