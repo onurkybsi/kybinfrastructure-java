31d6364db4faa72d5e8455da6993486a20e27541
Onur Kayabasi <o.kayabasi@21.finance>
2023-12-10 10:48:54 +0100

    test: KybContainerBuilder tests refactored

--- core/src/test/java/org/kybinfrastructure/ioc/KybContainerBuilderTest.java
+++ core/src/test/java/org/kybinfrastructure/ioc/KybContainerBuilderTest.java
@@ -1,30 +1,13 @@
 package org.kybinfrastructure.ioc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import example.service.SomeService;
-import example.service.sub.AnotherService;
 
 class KybContainerBuilderTest {
 
   @Test
-  void kybContainerBuilder_Throws_IllegalArgumentException_When_GivenRootClass_IsNull() {
-    // given
-    Class<?> rootClass = null;
-
-    // when
-    IllegalArgumentException thrownException =
-        assertThrows(IllegalArgumentException.class, () -> new KybContainerBuilder(rootClass));
-
-    // then
-    assertEquals("rootClass cannot be null!", thrownException.getMessage());
-  }
-
-  @Test
-  void build_Builds_Fresh_KybContainer_With_Given_Properties() {
+  void build_Builds_Fresh_KybContainer_With_Set_Properties() {
     // given
     Class<SomeService> rootClass = SomeService.class;
     KybContainerBuilder builder = new KybContainerBuilder(rootClass);
@@ -34,10 +17,6 @@ class KybContainerBuilderTest {
 
     // then
     assertNotNull(actualResult);
-    var managedClasses = actualResult.getManagedClasses();
-    assertEquals(2, managedClasses.size());
-    assertTrue(managedClasses.stream().anyMatch(mc -> SomeService.class.isAssignableFrom(mc)));
-    assertTrue(managedClasses.stream().anyMatch(mc -> AnotherService.class.isAssignableFrom(mc)));
   }
 
 }
