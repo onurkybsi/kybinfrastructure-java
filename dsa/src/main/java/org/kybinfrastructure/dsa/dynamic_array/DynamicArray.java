d4eaa5714df0a3e5df5b470091c58d80db8beee9
Onur Kayabasi <o.kayabasi@outlook.com>
2024-03-07 18:54:58 +0300

    Some DynamicArray functionalities added

--- dsa/src/main/java/org/kybinfrastructure/dsa/dynamic_array/DynamicArray.java
+++ dsa/src/main/java/org/kybinfrastructure/dsa/dynamic_array/DynamicArray.java
@@ -10,10 +10,13 @@ import java.util.Arrays;
  */
 public final class DynamicArray<T> {
 
-  private static final float GROWTH_RATE = 1.5F; // TODO: What is the optimal value?
+  private static final int DEFAULT_CAPACITY = 10;
+  private static final int GROWTH_FACTOR = 2;
+  private static final float SHRINK_THRESHOLD = 0.75F;
 
-  private int capacity = 16; // TODO: What is the optimal value?
-  private int size = 0;
+  private int capacity = DEFAULT_CAPACITY;
+  private int startIx = 0;
+  private int endIx = -1;
   private Object[] src;
 
   private DynamicArray() {
@@ -21,8 +24,9 @@ public final class DynamicArray<T> {
   }
 
   private DynamicArray(T[] elements) {
-    this.capacity = Math.round(elements.length * GROWTH_RATE);
-    this.size = elements.length;
+    this.capacity = elements.length * GROWTH_FACTOR;
+    this.startIx = 0;
+    this.endIx = elements.length - 1;
     this.src = Arrays.copyOf(elements, this.capacity);
   }
 
@@ -36,21 +40,119 @@ public final class DynamicArray<T> {
 
   @SuppressWarnings({"unchecked"})
   public T getAt(int ix) {
-    if (ix >= this.size) {
+    if (ix >= size()) {
       throw new IndexOutOfBoundsException("No element exists at index " + ix);
     }
-    return (T) this.src[ix];
+    return (T) this.src[startIx + ix];
   }
 
   public void setAt(int ix, T valueToSet) {
-    if (ix >= this.size) {
+    if (ix >= size()) {
       throw new IndexOutOfBoundsException("No element exists at index " + ix);
     }
-    this.src[ix] = valueToSet;
+    this.src[startIx + ix] = valueToSet;
+  }
+
+  public void insertFirst(T valueToInsert) {
+    int size = size();
+
+    if (size == capacity) {
+      int newCapacity = size * GROWTH_FACTOR;
+
+      Object[] grownSrc = new Object[newCapacity];
+      grownSrc[0] = valueToInsert;
+      for (int i = 0; i < size; i++) {
+        grownSrc[i + 1] = this.src[startIx + i];
+      }
+
+      this.capacity = newCapacity;
+      this.startIx = 0;
+      this.endIx = size;
+      this.src = grownSrc;
+    } else if (this.startIx > 0) {
+      this.startIx--;
+      this.src[startIx] = valueToInsert;
+    } else {
+      for (int i = size - 1; i >= 0; i--) {
+        this.src[i + 1] = this.src[i];
+      }
+      this.src[0] = valueToInsert;
+
+      this.endIx = size;
+    }
+  }
+
+  public void deleteFirst() {
+    int newSize = size() - 1;
+    if (newSize < 0) {
+      return;
+    }
+
+    if (this.capacity > DEFAULT_CAPACITY && ((float) newSize / this.capacity) < SHRINK_THRESHOLD) {
+      int newCapacity = Math.max(DEFAULT_CAPACITY, capacity / 2);
+      Object[] shrunkSource = new Object[newCapacity];
+      for (int i = 0; i < newSize; i++) {
+        shrunkSource[i] = this.src[startIx + 1 + i];
+      }
+
+      this.capacity = newCapacity;
+      this.startIx = 0;
+      this.endIx = newSize - 1;
+      this.src = shrunkSource;
+    } else {
+      this.startIx++;
+    }
+  }
+
+  public void insertLast(T valueToInsert) {
+    int size = size();
+
+    if (size == capacity) {
+      int newCapacity = size * GROWTH_FACTOR;
+
+      Object[] grownSrc = new Object[newCapacity];
+      for (int i = 0; i < size; i++) {
+        grownSrc[i] = this.src[startIx + i];
+      }
+      grownSrc[size] = valueToInsert;
+
+      this.capacity = newCapacity;
+      this.startIx = 0;
+      this.endIx = size;
+      this.src = grownSrc;
+    } else {
+      this.endIx++;
+      this.src[endIx] = valueToInsert;
+    }
+  }
+
+  public void deleteLast() {
+    int newSize = size() - 1;
+    if (newSize < 0) {
+      return;
+    }
+
+    if (this.capacity > DEFAULT_CAPACITY && ((float) newSize / this.capacity) < SHRINK_THRESHOLD) {
+      int newCapacity = Math.max(DEFAULT_CAPACITY, capacity / 2);
+      Object[] shrunkSource = new Object[newCapacity];
+      for (int i = 0; i < newSize; i++) {
+        shrunkSource[i] = this.src[startIx + i];
+      }
+
+      this.capacity = newCapacity;
+      this.startIx = 0;
+      this.endIx = newSize - 1;
+      this.src = shrunkSource;
+    } else {
+      this.endIx--;
+    }
   }
 
   public int size() {
-    return this.size;
+    if (this.endIx < 0) {
+      return 0;
+    }
+    return this.endIx - this.startIx + 1;
   }
 
 }
--- dsa/src/test/java/org/kybinfrastructure/dsa/dynamic_array/DynamicArrayTest.java
+++ dsa/src/test/java/org/kybinfrastructure/dsa/dynamic_array/DynamicArrayTest.java
@@ -36,4 +36,274 @@ final class DynamicArrayTest {
     assertEquals(5, actualResult.size());
   }
 
+  @Test
+  void getAt_Should_Return_Item_At_Given_Ix() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 2, 3, 4, 5});
+
+    // when
+    var actualResult = dynamicArray.getAt(3);
+
+    // then
+    assertEquals(4, actualResult);
+  }
+
+  @Test
+  void getAt_Should_Throw_IndexOutOfBoundsException_When_GivenIx_OutOf_UnderlyingArray() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 2, 3, 4, 5});
+
+    // when & then
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(5));
+  }
+
+  @Test
+  void setAt_Should_Set_Item_At_Given_Ix_By_GivenValue() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 2, 3, 4, 5});
+
+    // when
+    dynamicArray.setAt(3, 6);
+
+    // then
+    assertEquals(6, dynamicArray.getAt(3));
+  }
+
+  @Test
+  void setAt_Should_Throw_IndexOutOfBoundsException_When_GivenIx_OutOf_UnderlyingArray() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 2, 3, 4, 5});
+
+    // when & then
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.setAt(5, 6));
+  }
+
+  @Test
+  void insertFirst_Should_Add_GivenItem_IntoArrayStart_By_Increasing_Capacity_When_Capacity_IsFull() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {2});
+    dynamicArray.insertLast(3);
+
+    // when
+    dynamicArray.insertFirst(1);
+
+    // then
+    assertEquals(1, dynamicArray.getAt(0));
+    assertEquals(2, dynamicArray.getAt(1));
+    assertEquals(3, dynamicArray.getAt(2));
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(3));
+  }
+
+  @Test
+  void insertFirst_Should_Add_GivenItem_IntoArrayStart_By_Setting_StartIx_When_ThereIs_Room_At_Start_Of_UnderlyingArray() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 3, 4, 5});
+    dynamicArray.deleteFirst();
+
+    // when
+    dynamicArray.insertFirst(2);
+
+    // then
+    assertEquals(2, dynamicArray.getAt(0));
+    assertEquals(3, dynamicArray.getAt(1));
+    assertEquals(4, dynamicArray.getAt(2));
+    assertEquals(5, dynamicArray.getAt(3));
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(4));
+  }
+
+  @Test
+  void insertFirst_Should_Add_GivenItem_IntoArrayStart_By_Shifting_Items_When_ThereIs_NoRoom_At_Start_Of_UnderlyingArray() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {2, 3, 4, 5});
+
+    // when
+    dynamicArray.insertFirst(1);
+
+    // then
+    assertEquals(1, dynamicArray.getAt(0));
+    assertEquals(2, dynamicArray.getAt(1));
+    assertEquals(3, dynamicArray.getAt(2));
+    assertEquals(4, dynamicArray.getAt(3));
+    assertEquals(5, dynamicArray.getAt(4));
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(5));
+  }
+
+  @Test
+  void deleteFirst_Should_Not_Do_Something_When_Array_Has_NoItem() {
+    // given
+    var dynamicArray = DynamicArray.empty();
+
+    // when
+    dynamicArray.deleteFirst();
+
+    // then
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(0));
+  }
+
+  @Test
+  void deleteFirst_Should_Delete_FirstItem_By_Decreasing_Capacity_When_SizeCapacity_Rate_Is_Lower_Than_ShrinkThreshold() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 2, 3, 4, 5, 6});
+
+    // when
+    dynamicArray.deleteFirst();
+
+    // then
+    assertEquals(2, dynamicArray.getAt(0));
+    assertEquals(3, dynamicArray.getAt(1));
+    assertEquals(4, dynamicArray.getAt(2));
+    assertEquals(5, dynamicArray.getAt(3));
+    assertEquals(6, dynamicArray.getAt(4));
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(5));
+  }
+
+  @Test
+  void deleteFirst_Should_Delete_FirstItem_By_Setting_StartIx_When_SizeCapacity_Rate_IsNot_Lower_Than_ShrinkThreshold() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 2, 3, 4, 5, 6});
+    dynamicArray.deleteLast();
+
+    // when
+    dynamicArray.deleteFirst();
+
+    // then
+    assertEquals(2, dynamicArray.getAt(0));
+    assertEquals(3, dynamicArray.getAt(1));
+    assertEquals(4, dynamicArray.getAt(2));
+    assertEquals(5, dynamicArray.getAt(3));
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(4));
+  }
+
+  @Test
+  void deleteFirst_Should_Delete_FirstItem_By_Setting_StartIx_When_Capacity_Is_Lower_That_DefaultCapacity() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 2, 3});
+
+    // when
+    dynamicArray.deleteFirst();
+
+    // then
+    assertEquals(2, dynamicArray.getAt(0));
+    assertEquals(3, dynamicArray.getAt(1));
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(2));
+  }
+
+  @Test
+  void insertLast_Should_Add_GivenItem_IntoArrayEnd_By_Increasing_Capacity_When_Capacity_IsFull() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {2});
+    dynamicArray.insertFirst(1);
+
+    // when
+    dynamicArray.insertLast(3);
+
+    // then
+    assertEquals(1, dynamicArray.getAt(0));
+    assertEquals(2, dynamicArray.getAt(1));
+    assertEquals(3, dynamicArray.getAt(2));
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(3));
+  }
+
+  @Test
+  void insertLast_Should_Add_GivenItem_IntoArrayEnd_By_Setting_EndIx_When_ThereIs_Room_At_End_Of_UnderlyingArray() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 2, 3, 4});
+    dynamicArray.deleteLast();
+
+    // when
+    dynamicArray.insertLast(5);
+
+    // then
+    assertEquals(1, dynamicArray.getAt(0));
+    assertEquals(2, dynamicArray.getAt(1));
+    assertEquals(3, dynamicArray.getAt(2));
+    assertEquals(5, dynamicArray.getAt(3));
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(4));
+  }
+
+  @Test
+  void deleteLast_Should_Not_Do_Something_When_Array_Has_NoItem() {
+    // given
+    var dynamicArray = DynamicArray.empty();
+
+    // when
+    dynamicArray.deleteLast();
+
+    // then
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(0));
+  }
+
+  @Test
+  void deleteLast_Should_Delete_LastItem_By_Decreasing_Capacity_When_SizeCapacity_Rate_Is_Lower_Than_ShrinkThreshold() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 2, 3, 4, 5, 6});
+
+    // when
+    dynamicArray.deleteLast();
+
+    // then
+    assertEquals(1, dynamicArray.getAt(0));
+    assertEquals(2, dynamicArray.getAt(1));
+    assertEquals(3, dynamicArray.getAt(2));
+    assertEquals(4, dynamicArray.getAt(3));
+    assertEquals(5, dynamicArray.getAt(4));
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(5));
+  }
+
+  @Test
+  void deleteLast_Should_Delete_LastItem_By_Setting_EndIx_When_SizeCapacity_Rate_IsNot_Lower_Than_ShrinkThreshold() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 2, 3, 4, 5, 6});
+    dynamicArray.deleteFirst();
+
+    // when
+    dynamicArray.deleteLast();
+
+    // then
+    assertEquals(2, dynamicArray.getAt(0));
+    assertEquals(3, dynamicArray.getAt(1));
+    assertEquals(4, dynamicArray.getAt(2));
+    assertEquals(5, dynamicArray.getAt(3));
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(4));
+  }
+
+  @Test
+  void deleteLast_Should_Delete_LastItem_By_Setting_EndIx_When_Capacity_Is_Lower_That_DefaultCapacity() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {1, 2, 3});
+
+    // when
+    dynamicArray.deleteLast();
+
+    // then
+    assertEquals(1, dynamicArray.getAt(0));
+    assertEquals(2, dynamicArray.getAt(1));
+    assertThrows(IndexOutOfBoundsException.class, () -> dynamicArray.getAt(2));
+  }
+
+  @Test
+  void size_Should_Return_Zero_When_DynamicArray_Has_NoItem() {
+    // given
+    var dynamicArray = DynamicArray.empty();
+
+    // when
+    int actualResult = dynamicArray.size();
+
+    // then
+    assertEquals(0, actualResult);
+  }
+
+  @Test
+  void size_Should_Return_NumberOfItems_In_DynamicArray_When_DynamicArray_Has_SomeItems() {
+    // given
+    var dynamicArray = DynamicArray.from(new Integer[] {2, 3, 4, 5});
+    dynamicArray.insertFirst(1);
+
+    // when
+    int actualResult = dynamicArray.size();
+
+    // then
+    assertEquals(5, actualResult);
+  }
+
 }
